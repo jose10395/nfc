d3e509489f8915160fd1064c452f1879ae488ebf
unknown <jose_10395@hotmail.com>
2018-04-13 10:42:26 -0500

    d

--- nfc3.py
+++ nfc3.py
@@ -36,10 +36,13 @@ try:
 		if(len(data) > 0):
 		    for values in data:                            
                             #cursor.execute("insert into registro_acceso(fecha_registro_acceso,horario_id)values(now(),"+str(values[0])+")")
-                            #print(values)
+                            #print(values[0])
                             #print(str[0])
+                            id_1=""
+                            id_2=values[0]
+                            id_strr=id_1+str(id_2)
                             print(data)
-                            id_strr=(data)
+                            #id_strr=(data)
                             cursor.execute("insert into registro_acceso(fecha_registro_acceso,horario_id)values(now(),"+id_strr+")")
                             GPIO.output(36,GPIO.HIGH)
                             time.sleep(1)
